1f90f3cdf5137b90c03cbda044e9cdbd6dcddd37
XN Developer <xn-developer>
2018-05-03 14:55:51 +0000

    Getting metrics twice daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('send:alert')->daily();
-        $schedule->command('update:metrics')->hourly();
+        $schedule->command('update:metrics')->twiceDaily(1, 13);
     }
 
     /**
